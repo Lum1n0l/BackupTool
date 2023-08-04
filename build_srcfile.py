2636844e864309a71dfeeb19e4f65f59b71a24c9
Jake Parry <jakeparry@email.com>
2023-08-04 17:22:42 +0100

    Revert "Add Venv"
    This reverts commit 05641d5a0aa9b4f244699913889acf66a5dc979d.

--- build_srcfile.py
+++ build_srcfile.py
@@ -1,3 +1,1 @@
-# Script to prompt user for paths to files to be backed up - then build out a .txt list of directories to be fed into backuptool.
-
-print("Backup Directory List Builder!")
+# Script to prompt user for paths to files to be backed up - then build out a .txt list of directories to be fed into backuptool. 
