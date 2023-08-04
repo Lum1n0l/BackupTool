05641d5a0aa9b4f244699913889acf66a5dc979d
Jake Parry <jakeparry@email.com>
2023-08-04 17:15:18 +0100

    Add Venv

--- build_srcfile.py
+++ build_srcfile.py
@@ -1,1 +1,3 @@
-# Script to prompt user for paths to files to be backed up - then build out a .txt list of directories to be fed into backuptool. 
+# Script to prompt user for paths to files to be backed up - then build out a .txt list of directories to be fed into backuptool.
+
+print("Backup Directory List Builder!")
