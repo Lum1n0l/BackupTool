96da9035b0995c77fe149a3fd0bcd0d347bb1345
Jake <105429229+dajtp@users.noreply.github.com>
2023-10-17 20:46:31 +0100

    Update

--- build_srcfile.py
+++ build_srcfile.py
@@ -1,3 +1,3 @@
 # Script to prompt user for paths to files to be backed up - then build out a .txt list of directories to be fed into backuptool. 
 
-print("Lets Choose What To Backup...")
+print("Lets Choose What To Backup....")
