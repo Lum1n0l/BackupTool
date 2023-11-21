fbca80c5050d116627899b805499055dba9d623c
Jake <105429229+dajtp@users.noreply.github.com>
2023-11-21 23:14:42 +0000

    Update

--- build_srcfile.py
+++ build_srcfile.py
@@ -1,5 +1,7 @@
 # Script to prompt user for paths to files to be backed up - then build out a .txt list of directories to be fed into backuptool. 
 
+import sys
+
 print("Lets Choose What To Backup....")
 
 with open ("src_dirs.txt", "w") as file:
@@ -8,3 +10,7 @@ with open ("src_dirs.txt", "w") as file:
         file.write(src_dir + "\n")
         if input("Add Another Directory? Y/N \n") == "N":
             break
+
+print("Directories Saved To src_dirs.txt")
+print("Please Run backuptool.py To Backup Your Files")
+sys.exit()
