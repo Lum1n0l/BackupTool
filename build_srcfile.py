5bd9b85a3e30c8453e4a370ac06d9c228cdb99a1
Jake <105429229+dajtp@users.noreply.github.com>
2023-12-20 19:43:46 +0000

    Optimizations...

--- build_srcfile.py
+++ build_srcfile.py
@@ -1,20 +1,26 @@
 # Script to prompt user for paths to files to be backed up - then build out a .txt list of directories to be fed into backuptool. 
 
-try:
-    import sys
-    def build_srcfile():
+import sys
+
+def build_srcfile():
+    try:
         print("Lets Choose What To Backup....")
 
         with open ("src_dirs.txt", "w") as file:
             while True:
                 src_dir = input("Please Enter The Absolute Path To The Folder You Wish To Backup: \n")
                 file.write(src_dir + "\n")
-                if input("Add Another Directory? Y/N \n") == "N":
+                if input("Add Another Directory? Y/N \n") == "N" or "n":
+                    break
+                elif input("Add Another Directory? Y/N \n") == "Y" or "y":
+                    continue
+                else:
+                    print("Invalid Selection")
                     break
 
-        print("Directories Saved To src_dirs.txt")
-        print("Please Run backuptool.py To Backup Your Files")
-        sys.exit()
-except Exception as e:
-    print(f"Error: {e}")
-    pass
+            print("Directories Saved To src_dirs.txt")
+            print("Please Run backuptool.py To Backup Your Files")
+            sys.exit()
+    except Exception as e:
+        print(f"Error during backup: {e}")
+        pass
